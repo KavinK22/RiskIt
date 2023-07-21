fd3863c06a8f5be4f93d2a0edbb4ca963280ed29
KavinK22 <122186852+KavinK22@users.noreply.github.com>
2023-07-20 21:01:30 -0400

    Create RiskIt_lnx.py

---
+++ RiskIt_lnx.py
@@ -0,0 +1,28 @@
+import os
+import random
+import string
+import socket
+
+def generate_random_string(length):
+    letters = string.ascii_letters
+    random_string = ''.join(random.choice(letters) for i in range(length))
+    return random_string
+
+while (True):
+    difficulty = input("Choose a difficulty (easy, medium, hard): ")
+    if (difficulty == "easy"):
+        x = bool(random.getrandbits(1))
+        usrInput = bool(input("Choose a boolean, true or false: "))
+        break
+    elif (difficulty == "medium"):
+        x = random.randint(1, 10)
+        usrInput = int(input("Choose an integer, 1 - 10: "))
+        break
+    elif (difficulty == "hard"):
+        x = generate_random_string(random.randint(100000, 1000000))
+        usrInput = input("Choose a string of letters, of length 100000 - 1000000: ")
+        break
+if (usrInput == x):
+    print("You chose right!")
+else:
+    print(":3 \n ", socket.getHostName(), socket.gethostbyname(hostname))
